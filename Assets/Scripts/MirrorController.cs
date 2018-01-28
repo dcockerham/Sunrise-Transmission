7ac4f8444b1f135df5bfe878378c93a7b9d71602
dcockerham <dylantchaos@gmail.com>
2018-01-27 17:25:32 -0800

    - can switch between mirror, torch, and prism

--- Assets/Scripts/MirrorController.cs
+++ Assets/Scripts/MirrorController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MirrorController : Obstacle {
 
+	public bool startAlive = false;
+	protected Transform transformTarget;
+
 	public float rotateSpeed = 30.0f;
 	public float speed = 15.0f;
 	protected bool selected;
@@ -11,6 +14,7 @@ public class MirrorController : Obstacle {
 
 	public Vector2 range = new Vector2(3f, 3f);
 	protected Vector3 startPosition;
+	protected Vector3 startScale;
 
 	protected SpriteRenderer spriteImage;
 
@@ -26,47 +30,78 @@ public class MirrorController : Obstacle {
 
 	public float displayAngle; //just for testing
 
+	public enum ObjectType
+	{
+		MIRROR_TYPE,
+		TORCH_TYPE,
+		PRISM_TYPE,
+	};
+	public ObjectType myType = ObjectType.MIRROR_TYPE;
+
+	public MirrorController switchForm;
+
 	// Use this for initialization
 	public override void Start () 
 	{
 		base.Start ();
 		selected = false;
-		startPosition = transform.position;
-		spriteImage = GetComponent<SpriteRenderer> ();
+
+		if (switchForm) {
+			transformTarget = transform.parent;
+		} else {
+			transformTarget = transform;
+		}
+
+		startPosition = transformTarget.position;
+		startScale = transformTarget.localScale;
+		spriteImage = GetComponentInChildren<SpriteRenderer> ();
 		lightBeams = GetComponentsInChildren<LightBehavior> ();
+
+		if (!startAlive)
+			gameObject.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		lightsActive = alwaysOn;
-		float x = transform.position.x;
-		float y = transform.position.y;
+		float x = transformTarget.position.x;
+		float y = transformTarget.position.y;
 
 		if (selected) {
 			if (-range.x < (x - startPosition.x)) {
 				//translate; up down left right
 				if (Input.GetKey (KeyCode.LeftArrow)) 
-					transform.position += new Vector3 (-0.2f * speed * Time.deltaTime, 0.0f, 0.0f);
+					transformTarget.position += new Vector3 (-0.2f * speed * Time.deltaTime, 0.0f, 0.0f);
 			}
 			if ((x - startPosition.x) < range.x) {
 				if (Input.GetKey (KeyCode.RightArrow)) 
-					transform.position += new Vector3 (0.2f * speed * Time.deltaTime, 0.0f, 0.0f);
+					transformTarget.position += new Vector3 (0.2f * speed * Time.deltaTime, 0.0f, 0.0f);
 			}
 			if (-range.y < (y - startPosition.y)) {
 				if (Input.GetKey (KeyCode.DownArrow))
-					transform.position += new Vector3 (0.0f, -0.2f * speed * Time.deltaTime, 0.0f);
+					transformTarget.position += new Vector3 (0.0f, -0.2f * speed * Time.deltaTime, 0.0f);
 			}
 			if ((y - startPosition.y) < range.y) {
 				if (Input.GetKey (KeyCode.UpArrow)) 
-					transform.position += new Vector3 (0.0f, 0.2f * speed * Time.deltaTime, 0.0f);
+					transformTarget.position += new Vector3 (0.0f, 0.2f * speed * Time.deltaTime, 0.0f);
 			}
 
 			//rotation; 9 / 0 on top bar
 			if (Input.GetKey (KeyCode.Alpha9)) {
-				transform.Rotate (new Vector3 (0.0f, 0.0f, rotateSpeed * Time.deltaTime));
+				transformTarget.Rotate (new Vector3 (0.0f, 0.0f, rotateSpeed * Time.deltaTime));
 			}
 			if (Input.GetKey (KeyCode.Alpha0)) {
-				transform.Rotate (new Vector3 (0.0f, 0.0f, -rotateSpeed * Time.deltaTime));
+				transformTarget.Rotate (new Vector3 (0.0f, 0.0f, -rotateSpeed * Time.deltaTime));
+			}
+
+			// change objects if you hit spacebar
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				if (switchForm) {
+					//transformObject.SetActive (true);
+					switchForm.gameObject.SetActive(true);
+					switchForm.selected = true;
+					gameObject.SetActive (false);
+				}
 			}
 		}
 
@@ -74,12 +109,12 @@ public class MirrorController : Obstacle {
 		//activate & deactive selected boolean;
 		if (mouseOn == true && Input.GetMouseButton(0) && selected == false) {
 			selected = true;
-			transform.localScale *= 2.0f;
+			transformTarget.localScale = 1.3f * startScale;
 		}
 
 		if (mouseOn == false && Input.GetMouseButton(0) && selected == true) {
 			selected = false;
-			transform.localScale *= 0.5f;
+			transformTarget.localScale = startScale;
 		}
 	}
 
@@ -99,13 +134,13 @@ public class MirrorController : Obstacle {
 	void OnMouseEnter(){
 		mouseOn = true;
 		if(!selected)
-			transform.localScale *= 1.2f;
+			transformTarget.localScale = 1.1f * startScale;
 		//Debug.Log ("Mouse now Enter");
 	}
 
 	void OnMouseExit(){
 		if(!selected)
-			transform.localScale *= 1f/1.2f;
+			transformTarget.localScale = startScale;
 		mouseOn = false;
 		//Debug.Log ("Mouse now Exit");
 	}
@@ -113,27 +148,24 @@ public class MirrorController : Obstacle {
 	public override void HitByLight(Vector3 lightOrigin)
 	{
 		Vector3 lightVec = transform.position - lightOrigin;
-		float angleBetween = Vector3.SignedAngle (lightVec.normalized, transform.up, Vector3.up);
+		float angleBetween = Vector3.SignedAngle (lightVec.normalized, transformTarget.up, Vector3.up);
 		if (angleBetween <= (correctAngle + angleVariance/2) && angleBetween >= (correctAngle - angleVariance/2)) 
 		{
 			lightsActive = true;
 		}
 		displayAngle = angleBetween;
 
-		if (realReflection) 
-		{
-			Vector3 reflectionAngle = Vector3.Reflect (lightVec.normalized, transform.up);
-			for (int i = 0; i < lightBeams.Length; i++) 
-			{
+		if (realReflection) {
+			Vector3 reflectionAngle = Vector3.Reflect (lightVec.normalized, transformTarget.up);
+			for (int i = 0; i < lightBeams.Length; i++) {
 				lightBeams [i].useReflection = true;
 				lightBeams [i].reflectionAngle = reverseReflection ? -reflectionAngle : reflectionAngle;
+			}
+		} else {
+			for (int i = 0; i < lightBeams.Length; i++) {
+				lightBeams [i].useReflection = false;
 			}
 		}
 	}
 
-	void OnTriggerEnter2D(Collider2D other) {
-		if (other.gameObject.tag == "lightBeam") {
-			Debug.Log ("touches lightbeam");
-		}
-	}
 }
